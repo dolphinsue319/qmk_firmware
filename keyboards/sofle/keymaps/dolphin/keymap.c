31c3e057fbb26897fbcd7db30c0603ab68a518b1
DolphinSu <dolphinsue@mail2000.com.tw>
2020-08-23 14:52:39 +0800

    alt 放在兩邊小指

--- keyboards/sofle/keymaps/dolphin/keymap.c
+++ keyboards/sofle/keymaps/dolphin/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |Ctrl+Space|   Z  |   X  |   C  |   V  |   B  | Mute |    | Sleep |   N  |   M  |   ,  |   .  |   /  |CAPSLOCK|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | COPY. alt |PASTE, ctrl | /Enter, command  /       \Space, command \  | Left | Up | Down | Right |
+ *            | LGUI | Lbf | COPY. alt |PASTE, ctrl | /Enter, command  /       \Space, command \  | Left | Up | Down | Right |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT( \
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
-  KC_LSHIFT,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
+  KC_LALT,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  RALT_T(KC_QUOT), \
   LCTL(KC_SPC), KC_Z, KC_X,  KC_C,    KC_V,    KC_B, KC_MUTE,     RGUI(RCTL(KC_Q)), KC_N,  KC_M,    KC_COMM,  KC_DOT, KC_SLSH,  KC_CAPSLOCK, \
     KC_UNDO, KC_CUT, LCTL_T(KC_COPY), LSFT_T(KC_PASTE), LCMD_T(KC_ENT),      RCMD_T(KC_SPC),  RSFT_T(KC_LEFT), KC_UP, KC_DOWN, KC_RIGHT \
 )
