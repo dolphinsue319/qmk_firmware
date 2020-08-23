c7a5672f3944eea1552693ebc9914b1dde63ce84
DolphinSu <dolphinsue@mail2000.com.tw>
2020-08-23 14:42:15 +0800

    右旋鈕用來輸入左右鍵

--- keyboards/sofle/keymaps/dolphin/keymap.c
+++ keyboards/sofle/keymaps/dolphin/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
   KC_LSHIFT,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
   LCTL(KC_SPC), KC_Z, KC_X,  KC_C,    KC_V,    KC_B, KC_MUTE,     RGUI(RCTL(KC_Q)), KC_N,  KC_M,    KC_COMM,  KC_DOT, KC_SLSH,  KC_CAPSLOCK, \
-    KC_UNDO, KC_CUT, LCTL_T(KC_COPY), LSFT_T(KC_PASTE), LCMD_T(KC_ENT),      RCMD_T(KC_SPC),  KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT \
+    KC_UNDO, KC_CUT, LCTL_T(KC_COPY), LSFT_T(KC_PASTE), LCMD_T(KC_ENT),      RCMD_T(KC_SPC),  RSFT_T(KC_LEFT), KC_UP, KC_DOWN, KC_RIGHT \
 )
 };
 
@@ -120,9 +120,9 @@ void encoder_update_user(uint8_t index, bool clockwise) {
         }
     } else if (index == 1) {
         if (clockwise) {
-            tap_code(KC_PGDOWN);
+            tap_code(KC_RIGHT);
         } else {
-            tap_code(KC_PGUP);
+            tap_code(KC_LEFT);
         }
     }
 }
