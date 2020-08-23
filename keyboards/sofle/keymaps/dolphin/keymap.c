9d1e93629500a215e0a6fa6faccf81f6a1dce513
DolphinSu <dolphinsue@mail2000.com.tw>
2020-08-23 12:04:08 +0800

    方向鍵及左右邊的 ctrl, alt

--- keyboards/sofle/keymaps/dolphin/keymap.c
+++ keyboards/sofle/keymaps/dolphin/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |Ctrl+Space|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |CAPSLOCK|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | COPY |PASTE | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            | LGUI | LAlt | COPY. alt |PASTE, ctrl | /Enter, command  /       \Space, command \  | Left | Up | Down | Right |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
   LCTL(KC_SPC),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_CAPSLOCK, \
-                 KC_LGUI,KC_LALT,KC_COPY, KC_PASTE, LCMD_T(KC_ENT),      RCMD_T(KC_SPC),  KC_CAPSLOCK, KC_RCTRL, KC_RALT, KC_RGUI \
+     KC_LGUI,KC_LALT,LALT_T(KC_COPY), LCTL_T(KC_PASTE), LCMD_T(KC_ENT),      RCMD_T(KC_SPC),  RCTL_T(KC_LEFT), RALT_T(KC_UP), KC_DOWN, KC_RIGHT \
 )
 };
 
