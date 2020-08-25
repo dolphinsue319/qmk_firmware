7f493bab9d966b884bce81af92e6e3acaba36350
DolphinSu <dolphinsue@mail2000.com.tw>
2020-08-25 17:13:13 +0800

    右下角改成複製貼上

--- keyboards/sofle/keymaps/dolphin/keymap.c
+++ keyboards/sofle/keymaps/dolphin/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_NONUS_BSLASH, \
   KC_LALT,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  RALT_T(KC_BSPC), \
   LCTL(KC_SPC), KC_Z, KC_X,  KC_C,    KC_V,    KC_B, KC_MUTE,     RGUI(RCTL(KC_Q)), KC_N,  KC_M,    KC_COMM,  KC_DOT, KC_SLSH,  KC_CAPSLOCK, \
-    KC_UNDO, KC_CUT, LCTL_T(KC_COPY), LSFT_T(KC_PASTE), LCMD_T(KC_ENT),      RCMD_T(KC_SPC),  RSFT_T(KC_LEFT), KC_UP, KC_DOWN, KC_RIGHT \
+    KC_UNDO, KC_CUT, LCMD(KC_C), LCMD(KC_V), LCMD_T(KC_ENT),      RCMD_T(KC_SPC),  RSFT_T(KC_LEFT), KC_UP, KC_DOWN, KC_RIGHT \
 )
 };
 
